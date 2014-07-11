094c5b49cc06ff884c7d593ca3897be69722b328
William Orr <will@worrbase.com>
2014-07-10 18:32:02 -0700

    autopep8

--- scripts/extract_sysops_api_to_disk.py
+++ scripts/extract_sysops_api_to_disk.py
@@ -19,32 +19,38 @@ import signal
 import gc
 import datetime
 
+
 class timeout_exception(Exception):
-  pass
+    pass
+
 
 def timeout_handler(signum, frame):
     raise timeout_exception()
 
-######################################################################################
-def execute_range_query():
-  range_query = "%cf3.promises.all-mps-cores"
-  range_servers = []
-  cm_conf = open("/etc/cm.conf", 'r')
-  for line in cm_conf.readlines():
-    if "MPS" in line:
-      range_servers.append(line.split(':')[1].rstrip())
+##########################################################################
 
-  while range_servers:
-    try:
-      range_server = range_servers.pop()
-      range_connection = seco.range.Range(range_server)
-      total_redis_corelist = range_connection.expand(range_query)
-      if total_redis_corelist:
-        break
-    except:
-      pass
-  return(total_redis_corelist)
-######################################################################################
+
+def execute_range_query():
+    range_query = "%cf3.promises.all-mps-cores"
+    range_servers = []
+    cm_conf = open("/etc/cm.conf", 'r')
+    for line in cm_conf.readlines():
+        if "MPS" in line:
+            range_servers.append(line.split(':')[1].rstrip())
+
+    while range_servers:
+        try:
+            range_server = range_servers.pop()
+            range_connection = seco.range.Range(range_server)
+            total_redis_corelist = range_connection.expand(range_query)
+            if total_redis_corelist:
+                break
+        except:
+            pass
+    return(total_redis_corelist)
+##########################################################################
+
+
 def main():
     base_directory = "/mnt/u001/sysops-api/"
 
@@ -55,64 +61,70 @@ def main():
     total_redis_corelist = execute_range_query()
 
     for core in total_redis_corelist:
-      dc = core.split('.')[2].split('-')[0]
-      sites[dc.upper()] = 1
+        dc = core.split('.')[2].split('-')[0]
+        sites[dc.upper()] = 1
 
     files_written = {}
     for datacenter in sites.iterkeys():
-      start_dc_time = time.time()
-      if not os.path.exists(base_directory + datacenter):
-        os.makedirs(base_directory + datacenter)
+        start_dc_time = time.time()
+        if not os.path.exists(base_directory + datacenter):
+            os.makedirs(base_directory + datacenter)
 
-      # Find the unique files for the datacenter.  Spawn a CacheExtractor object for every key.  Dumping the entire datacenter in a single object requires too much RAM.
-      uniqueFiles = CacheExtractor.CacheExtractor(scope = 'site', site = datacenter.lower(), list_files = True)
+        # Find the unique files for the datacenter.  Spawn a CacheExtractor
+        # object for every key.  Dumping the entire datacenter in a single
+        # object requires too much RAM.
+        uniqueFiles = CacheExtractor.CacheExtractor(
+            scope='site', site=datacenter.lower(), list_files=True)
 
-      number_of_keys = len(uniqueFiles._gold.keys())
-      keys_processed = 0
-      for key in sorted(uniqueFiles._gold.iterkeys()):
-        keys_processed += 1
-        start_file_time = time.time()
-        filename = key.split("#")[1]
-        # Spawn a CacheExtractor object for every unique key, in every datacenter.
-        try:
-          dataDump =  CacheExtractor.CacheExtractor(scope = 'site', site = datacenter.lower(), search_string = filename, contents = True)
-        except Exception, e:
-          print "Lost filename " + filename + " The object has dropped out of the cache"
-          continue
+        number_of_keys = len(uniqueFiles._gold.keys())
+        keys_processed = 0
+        for key in sorted(uniqueFiles._gold.iterkeys()):
+            keys_processed += 1
+            start_file_time = time.time()
+            filename = key.split("#")[1]
+            # Spawn a CacheExtractor object for every unique key, in every
+            # datacenter.
+            try:
+                dataDump = CacheExtractor.CacheExtractor(
+                    scope='site', site=datacenter.lower(), search_string=filename, contents=True)
+            except Exception, e:
+                print "Lost filename " + filename + " The object has dropped out of the cache"
+                continue
 
-        for key in sorted(dataDump._gold.iterkeys()):
-          host, file = key.split('#')
-          hostdir = base_directory + datacenter + "/" + host
-          if not os.path.exists(hostdir + os.path.dirname(file)):
-            os.makedirs(hostdir + os.path.dirname(file))
-          if dataDump._gold[key]:
-            tmp_target_file_name = hostdir + os.path.dirname(file) + "/." + os.path.basename(file)
-            target_file_name = hostdir + file
-            tmp_target_file = open(tmp_target_file_name, 'w')
-            tmp_target_file.write(dataDump._gold[key])
-	    tmp_target_file.close()
-	    shutil.move(tmp_target_file_name, target_file_name)
-            files_written[target_file_name] = 1
-        del dataDump
+            for key in sorted(dataDump._gold.iterkeys()):
+                host, file = key.split('#')
+                hostdir = base_directory + datacenter + "/" + host
+                if not os.path.exists(hostdir + os.path.dirname(file)):
+                    os.makedirs(hostdir + os.path.dirname(file))
+                if dataDump._gold[key]:
+                    tmp_target_file_name = hostdir + \
+                        os.path.dirname(file) + "/." + os.path.basename(file)
+                    target_file_name = hostdir + file
+                    tmp_target_file = open(tmp_target_file_name, 'w')
+                    tmp_target_file.write(dataDump._gold[key])
+                    tmp_target_file.close()
+                    shutil.move(tmp_target_file_name, target_file_name)
+                    files_written[target_file_name] = 1
+            del dataDump
+            gc.collect()
+            print "Completed filename " + filename + " in datacenter " + datacenter + " in " + str(int(time.time() - start_file_time)) + " seconds. " + str(number_of_keys - keys_processed) + " keys remain for the datacenter."
+            print "Total time elapsed is " + str(datetime.timedelta(seconds=int(time.time() - start_time)))
+
+        del uniqueFiles
         gc.collect()
-        print "Completed filename " + filename + " in datacenter " + datacenter + " in " + str(int(time.time() - start_file_time)) + " seconds. " + str(number_of_keys - keys_processed) + " keys remain for the datacenter."
-        print "Total time elapsed is " + str(datetime.timedelta(seconds=int(time.time() - start_time)))
-
-      del uniqueFiles  
-      gc.collect()
-      print "Completed datacenter " + datacenter + " in " + str(int(time.time() - start_dc_time)) + " seconds"
+        print "Completed datacenter " + datacenter + " in " + str(int(time.time() - start_dc_time)) + " seconds"
 
     purge_start = time.time()
     print "Starting purge of stale data at " + time.ctime()
     files_on_disk = {}
     for (path, dirs, files) in os.walk(base_directory):
-      for file in files:
-        files_on_disk[path + "/" + file] = 1
-        
+        for file in files:
+            files_on_disk[path + "/" + file] = 1
+
     for file in files_on_disk.iterkeys():
-      if not files_written.get(file):
-        print "removing stale file " + file
-        os.remove(file)
+        if not files_written.get(file):
+            print "removing stale file " + file
+            os.remove(file)
     print "Purge of stale data completed in " + str(int(time.time() - purge_start)) + " seconds"
 
     print "Completed execution in " + str(int(time.time() - start_time)) + " seconds"
@@ -120,19 +132,20 @@ def main():
     seconds_left = 1200 - int(time.time() - start_time)
     print "Sleeping for " + str(seconds_left) + " seconds"
     if seconds_left > 0:
-      time.sleep(seconds_left)
-          
+        time.sleep(seconds_left)
+
 if __name__ == '__main__':
 
-  # Set a 60 minute alarm
-  old_handler = signal.signal(signal.SIGALRM, timeout_handler) 
-  # esv4-infra07 takes a long time to purge stale data.  Set this timeout very high until something else can be figured out.
-  signal.alarm(21600) 
-  "(+) Starting 6 hour signal alarm"
-  try:
-    main()
-  except timeout_exception:
-    print "6 hours exceeded.  Bailing"
-  finally:
-    signal.signal(signal.SIGALRM, old_handler)
-    signal.alarm(0)
+    # Set a 60 minute alarm
+    old_handler = signal.signal(signal.SIGALRM, timeout_handler)
+    # esv4-infra07 takes a long time to purge stale data.  Set this timeout
+    # very high until something else can be figured out.
+    signal.alarm(21600)
+    "(+) Starting 6 hour signal alarm"
+    try:
+        main()
+    except timeout_exception:
+        print "6 hours exceeded.  Bailing"
+    finally:
+        signal.signal(signal.SIGALRM, old_handler)
+        signal.alarm(0)
--- scripts/extract_user_sudo_privileges.py
+++ scripts/extract_user_sudo_privileges.py
@@ -11,62 +11,63 @@ import sys
 sys.path.append("/usr/local/admin")
 import CacheExtractor
 
-###############################################################################################
+##########################################################################
+
+
 def main():
-  parser = OptionParser(usage ="usage: %prog [options]",
-    version ="%prog 1.0") 
-  parser.add_option("--verbose",
-    action = "store_true",
-    dest = "verbose",
-    default = False,
-    help = "Enable verbose execution")
-  parser.add_option("--range-query",
-    action = "store",
-    dest = "range_query",
-    help = "Specify a range cluster of hosts you which to use to use to make queries against.")
-  parser.add_option("--user",
-    action = "store",
-    dest = "user",
-    help = "Specify a unix user uid or id that you are interested in searching for.")
+    parser = OptionParser(usage="usage: %prog [options]",
+                          version="%prog 1.0")
+    parser.add_option("--verbose",
+                      action="store_true",
+                      dest="verbose",
+                      default=False,
+                      help="Enable verbose execution")
+    parser.add_option("--range-query",
+                      action="store",
+                      dest="range_query",
+                      help="Specify a range cluster of hosts you which to use to use to make queries against.")
+    parser.add_option("--user",
+                      action="store",
+                      dest="user",
+                      help="Specify a unix user uid or id that you are interested in searching for.")
 
-  (options,args) = parser.parse_args()
+    (options, args) = parser.parse_args()
 
+    ina_groups = []
+    ina_group_file = open(
+        '/etc/sudo.d/sudoers-USERS_GROUP_WORLD_READABLE', 'r').readlines()
+    for line in ina_group_file:
+        if options.user in line:
+            ina_groups.append(line.split()[1])
 
-  ina_groups = []
-  ina_group_file = open('/etc/sudo.d/sudoers-USERS_GROUP_WORLD_READABLE', 'r').readlines()
-  for line in ina_group_file:
-    if options.user in line:
-      ina_groups.append(line.split()[1])
-      
+    sudoers_rules = {}
+    sudoers_file = open('/etc/sudo.d/sudoers_WORLD_READABLE', 'r').readlines()
+    for line in sudoers_file:
+        if " = " in line:
+            ina_group = line.split()[0].strip()
+            machine_group = line.split()[1].strip()
+            privs = line.split('=')[1].strip()
 
-  sudoers_rules = {}
-  sudoers_file = open('/etc/sudo.d/sudoers_WORLD_READABLE', 'r').readlines()
-  for line in sudoers_file:
-    if " = " in line:
-      ina_group = line.split()[0].strip()
-      machine_group = line.split()[1].strip()
-      privs = line.split('=')[1].strip()
+            if ina_group in ina_groups:
+                sudoers_rules[machine_group] = privs
 
-      if ina_group in ina_groups:
-        sudoers_rules[machine_group] = privs
+    cache_results = CacheExtractor.CacheExtractor(verbose=options.verbose,
+                                                  scope='global',
+                                                  contents=True,
+                                                  range_query=options.range_query,
+                                                  search_string='sudoers-MACHINE_GROUP')
 
-  
-  cache_results = CacheExtractor.CacheExtractor(verbose = options.verbose,
-						scope = 'global',
-						contents = True,
-						range_query = options.range_query,
-						search_string = 'sudoers-MACHINE_GROUP')
+    for key in cache_results._gold.iterkeys():
+        host = key.split('#')[0]
+        for line in cache_results._gold[key].splitlines():
+            if "Host_Alias" in line:
+                system_machine_group = line.split()[1]
+                if sudoers_rules.get(system_machine_group):
+                    print ("user: " + options.user).ljust(0) + \
+                          ("privs: " + sudoers_rules.get(system_machine_group)).center(40) + \
+                          ("host: " + host).center(40) + \
+                          ("machine_group: " + system_machine_group).rjust(10)
 
-  for key in cache_results._gold.iterkeys():
-    host = key.split('#')[0]
-    for line in cache_results._gold[key].splitlines():
-      if "Host_Alias" in line:
-        system_machine_group = line.split()[1]
-        if sudoers_rules.get(system_machine_group):
-          print ("user: " + options.user).ljust(0) + \
-                ("privs: " + sudoers_rules.get(system_machine_group)).center(40) + \
-		("host: " + host).center(40) + \
-                ("machine_group: " + system_machine_group).rjust(10)
-        
-###############################################################################################
-if __name__ == '__main__': main()
+##########################################################################
+if __name__ == '__main__':
+    main()
